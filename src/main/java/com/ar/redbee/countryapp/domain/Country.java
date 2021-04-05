4ae97fe7800a7cc7f9929f4b466fd6840b4be651
ariel <ariel.ep91@gmail.com>
2021-04-05 00:27:30 -0300

    clase del dominio inmutable

--- src/main/java/com/ar/redbee/countryapp/domain/Country.java
+++ src/main/java/com/ar/redbee/countryapp/domain/Country.java
@@ -3,14 +3,14 @@ package com.ar.redbee.countryapp.domain;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Value;
 
-@Data
+@Value
 @Builder
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Country {
-    private String name;
-    private String alpha2Code;
-    private String alpha3Code;
-    private String capital;
+    String name;
+    String alpha2Code;
+    String alpha3Code;
+    String capital;
 }
